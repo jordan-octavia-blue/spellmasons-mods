9b03770d89fb2ecb526e15cf0bd8f14e479cc579
Jordan O'Leary <jdoleary@gmail.com>
2025-10-23 11:25:07 -0400

    mod: Add Doriosos_minions

---
+++ Doriosos_minions/doriosos_minions.ts
@@ -0,0 +1,14 @@
+import type { Mod } from '../types/types/commonTypes';
+
+
+
+const mod: Mod = {
+    modName: 'Dorioso\'s Minions',
+    author: 'Dorioso Aytario',
+    description: 'Adds familiars that accompany you.',
+    screenshot: 'images/empty.png',
+    spritesheet: 'spellmasons-mods/Doriosos_minions/spritesheet/sheet.json',
+    familiars: ['beholder', 'shroom', 'goo'],
+
+};
+export default mod;
